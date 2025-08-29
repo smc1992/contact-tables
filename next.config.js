2c9a22647447e8405781a33a74e8208df31cb742
Cascade Assistant <cascade@example.com>
2025-08-29 19:31:45 +0200

    Fix: rc-pagination zu next-transpile-modules hinzugefügt und webpack-Konfiguration für ES6-Module verbessert

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')(['rc-util', 'rc-picker', '@rc-component/util', '@rc-component/trigger', 'rc-table', 'rc-tree', 'rc-select', 'rc-dropdown', 'rc-menu', 'rc-motion', 'rc-notification', 'rc-tooltip', 'rc-tree-select', '@ant-design/icons-svg']);
+const withTM = require('next-transpile-modules')(['rc-util', 'rc-picker', '@rc-component/util', '@rc-component/trigger', 'rc-table', 'rc-tree', 'rc-select', 'rc-dropdown', 'rc-menu', 'rc-motion', 'rc-notification', 'rc-tooltip', 'rc-tree-select', '@ant-design/icons-svg', 'rc-pagination']);
 
 const nextConfig = {
   reactStrictMode: true,
@@ -18,11 +18,25 @@ const nextConfig = {
     STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
   },
-  webpack: (config, { dev }) => {
+  webpack: (config, { dev, isServer }) => {
     // Disable webpack caching in development mode to troubleshoot build issues
     if (dev) {
       config.cache = false;
     }
+    
+    // Transpile ES6 modules to CommonJS for compatibility
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['next/babel']
+          }
+        }
+      });
+    }
+    
     return config;
   },
 }
